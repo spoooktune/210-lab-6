9de8eed0d46f7d4a716911870ec6bef814b484d8
spoooktune <asriprasana5@gmail.com>
2025-09-14 20:02:31 -0700

    Added enterArrayData func

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include <string>
 
 using namespace std;
+
+void enterArrayData(double *inputArr[]){
+    int size = sizeof(*inputArr);
+    for (int i = 0; i < size; i++){
+        cout << "Enter value for element " << i + 1 << ": " << endl;
+        cin >> *inputArr[i];
+    }
+}
+
+
+
 int main(){
-    
+
 }
