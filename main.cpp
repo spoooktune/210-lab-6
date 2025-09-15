c17d7a79e2d7d7474b2ffd952ecf887f0fad785e
spoooktune <asriprasana5@gmail.com>
2025-09-14 20:32:50 -0700

    added informative comments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
+// COMSC 210 | Lab 6 | Amrutha Sriprasana
+// Made w/ VSCode
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+// function to enter array's data
 void enterArrayData(double *inputArr, const int SIZE){
     cout << "Data entry for the array: " << endl;
     for (int i = 0; i < SIZE; i++){
@@ -11,6 +15,7 @@ void enterArrayData(double *inputArr, const int SIZE){
     }
 }
 
+// function to print array's data
 void outputArrayData(double *outputArr, const int SIZE){
     cout << "Outputting array elements: ";
     for (int i = 0; i < SIZE; i++){
@@ -19,6 +24,7 @@ void outputArrayData(double *outputArr, const int SIZE){
     cout << endl;
 }
 
+// function to calculate and outpu sum of array elements
 void sumArray(double *arrayPtr, const int SIZE){
     double sum = 0;
     for (int i = 0; i < SIZE; i++){
@@ -27,6 +33,7 @@ void sumArray(double *arrayPtr, const int SIZE){
     cout << "Sum of elements: " << sum << endl;
 }
 
+// create array and array's pointer, run all 3 above functions
 int main(){
     const int SIZE = 5;
     double array[SIZE];
