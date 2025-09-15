900348ebb30c2b605c7a16c104a8ce47663404ae
spoooktune <asriprasana5@gmail.com>
2025-09-14 20:12:41 -0700

    added outputArrayData and sumArray functions

--- main.cpp
+++ main.cpp
@@ -5,13 +5,29 @@ using namespace std;
 
 void enterArrayData(double *inputArr[]){
     int size = sizeof(*inputArr);
+    cout << "Data entry for the array: " << endl;
     for (int i = 0; i < size; i++){
-        cout << "Enter value for element " << i + 1 << ": " << endl;
+        cout << "> Element #" << i << ": " << endl;
         cin >> *inputArr[i];
     }
 }
 
+void outputArrayData(double *outputArr[]){
+    int size = sizeof(*outputArr);
+    cout << "Outputting array elements: ";
+    for (int i = 0; i < size; i++){
+        cout << *(outputArr + i);
+    }
+    cout << endl;
+}
 
+void sumArray(double *arrayPtr[]){
+    double sum = 0;
+    for (int i = 0; i < sizeof(*arrayPtr); i++){
+        sum += *arrayPtr[i];
+    }
+    cout << "Sum of elements: " << sum << endl;
+}
 
 int main(){
 
