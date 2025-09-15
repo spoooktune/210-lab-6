b410c240a699b15669fc9e7ee7c8beca1d81390c
spoooktune <asriprasana5@gmail.com>
2025-09-14 20:29:13 -0700

    added code to main(), added size parameter to all functions

--- main.cpp
+++ main.cpp
@@ -3,32 +3,36 @@
 
 using namespace std;
 
-void enterArrayData(double *inputArr[]){
-    int size = sizeof(*inputArr);
+void enterArrayData(double *inputArr, const int SIZE){
     cout << "Data entry for the array: " << endl;
-    for (int i = 0; i < size; i++){
-        cout << "> Element #" << i << ": " << endl;
-        cin >> *inputArr[i];
+    for (int i = 0; i < SIZE; i++){
+        cout << "> Element #" << i << ": ";
+        cin >> *(inputArr + i);
     }
 }
 
-void outputArrayData(double *outputArr[]){
-    int size = sizeof(*outputArr);
+void outputArrayData(double *outputArr, const int SIZE){
     cout << "Outputting array elements: ";
-    for (int i = 0; i < size; i++){
-        cout << *(outputArr + i);
+    for (int i = 0; i < SIZE; i++){
+        cout << *(outputArr + i) << " ";
     }
     cout << endl;
 }
 
-void sumArray(double *arrayPtr[]){
+void sumArray(double *arrayPtr, const int SIZE){
     double sum = 0;
-    for (int i = 0; i < sizeof(*arrayPtr); i++){
-        sum += *arrayPtr[i];
+    for (int i = 0; i < SIZE; i++){
+        sum += *(arrayPtr + i);
     }
     cout << "Sum of elements: " << sum << endl;
 }
 
 int main(){
-
+    const int SIZE = 5;
+    double array[SIZE];
+    double *arrPtr = nullptr;
+    arrPtr = array;
+    enterArrayData(arrPtr, SIZE);
+    outputArrayData(arrPtr, SIZE);
+    sumArray(arrPtr, SIZE);
 }
